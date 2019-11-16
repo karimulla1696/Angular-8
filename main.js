13089f650a50d0a3284bb7652276d9756b310d48
KarimullaAppScrip <raghavendra@mobifyi.com>
2019-11-16 10:36:03 +0530

    #typescript examples

--- main.js
+++ main.js
@@ -1,4 +1,111 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
-var message = 'Hello world';
+var message = 'welcome back';
 console.log(message);
+var x = 10;
+var y = 30;
+var sum;
+var title = 'my self karimulla';
+var isBeginner = true;
+var total = 0;
+var name = 'kareem';
+// template string
+var sentence = "My name " + name + "\nI am a beginner in TypeScript ";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var persons1 = ['kareem', 23];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Blue;
+console.log(c);
+// variable deceleration
+var randomValue = 10;
+randomValue = true;
+randomValue = 'kareem';
+var myVariable = 10;
+function hasName(obj) {
+    return !!obj && typeof obj === 'object' && "name" in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+myVariable.toUpperCase();
+// variable types
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multipleType;
+multipleType = 20;
+multipleType = true;
+var anyTime;
+anyTime = 20;
+anyTime = true;
+// functions
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+add(5);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'shaik'
+    // lastName: 'karimulla'
+};
+fullName(p);
+// classes
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('karimulla');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks ");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('manjoor');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- main.ts
+++ main.ts
@@ -1,3 +1,125 @@
 export {}
-let message = 'Hello world';
+let message = 'welcome back';
 console.log(message);
+
+let x = 10;
+const y = 30;
+
+let sum;
+const title = 'my self karimulla';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'kareem';
+
+// template string
+let sentence: string = `My name ${name}
+I am a beginner in TypeScript `;
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+let persons1: [string, number] = ['kareem', 23];
+
+enum Color {Red, Green, Blue};
+
+let c: Color = Color.Blue;
+console.log(c);
+
+// variable deceleration
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'kareem';
+
+let myVariable: unknown = 10;
+
+function hasName(obj: any): obj is { name: string } {
+    return !!obj && typeof obj === 'object' && "name" in obj
+}
+
+if(hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+(myVariable as string).toUpperCase();
+
+// variable types
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+
+let multipleType: number | boolean;
+multipleType = 20;
+multipleType = true;
+
+let anyTime: any;
+anyTime = 20;
+anyTime = true;
+
+
+// functions
+function add(num1: number, num2: number = 10): number {
+    if (num2)
+        return num1 + num2;
+        else
+            return num1
+}
+
+add(5, 10);
+add(5);
+
+// interfaces
+
+interface Person {
+    firstName: string;
+    lastName?: string;
+}
+function fullName(person: Person) {
+    console.log(`${person.firstName} ${person.lastName}`)
+}
+
+let p = {
+    firstName: 'shaik'
+    // lastName: 'karimulla'
+};
+
+fullName(p);
+
+// classes
+class Employee {
+    public employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good morning ${this.employeeName}`)
+    }
+}
+
+let emp1 = new Employee('karimulla');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+    delegateWork() {
+        console.log(`Manager delegating tasks `)
+    }
+}
+
+let m1 = new Manager('manjoor');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
