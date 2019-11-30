9ab94415cfcfb665974ac96c9aff22e72b5e9641
KarimullaAppScrip <raghavendra@mobifyi.com>
2019-11-30 17:45:55 +0530

    #Routing

--- angular8/src/app/auth-guard.service.ts
+++ angular8/src/app/auth-guard.service.ts
@@ -1,10 +1,17 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from "@angular/router";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {AuthService} from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
@@ -18,5 +25,8 @@ export class AuthGuard implements CanActivate{
           }
         }
       )
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
   }
 }
